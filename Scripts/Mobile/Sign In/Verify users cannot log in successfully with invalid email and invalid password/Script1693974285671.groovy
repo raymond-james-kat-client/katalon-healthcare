e5e3e99efc1b6f1f17861ff3430a696f40de930c
raymond-james-kat-client <raymondjames.kat.client@gmail.com>
2024-08-07 10:30:30 +0700

    adjust pwd step

--- Scripts/Mobile/Sign In/Verify users cannot log in successfully with invalid email and invalid password/Script1693974285671.groovy
+++ Scripts/Mobile/Sign In/Verify users cannot log in successfully with invalid email and invalid password/Script1693974285671.groovy
@@ -1,9 +1,11 @@
+import com.kms.katalon.util.CryptoUtil
+
 import katalon.android.common.SignInPage
 import katalon.fw.lib.Page
 
 Page.nav(SignInPage)
 	.setEmail(invalidEmail)
-	.setPassword(invalidPassword)
+	.setPassword(CryptoUtil.encode(CryptoUtil.getDefault(invalidPassword)))
 	.tapSignIn()
 	.verifyErrorMessage(emailErrorMessage)
 	.verifyErrorMessage(passwordErrorMessage)
